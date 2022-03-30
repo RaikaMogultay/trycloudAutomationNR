a036086cd689761f9d41e927aca299fd34ca1bd3
RaikaMogultay <raika.engin@gmail.com>
2022-03-30 17:59:01 -0500

    new commit

--- src/test/java/com/trycloud/pages/US06Page.java
+++ src/test/java/com/trycloud/pages/US06Page.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class US06Page {
+public class US06Page extends LoginPage{
 
     public US06Page(){
 
@@ -14,6 +14,18 @@ public class US06Page {
 
     @FindBy(xpath = "(//a[@aria-label='Files'])[1]")
     public WebElement filesButton;
+
+    @FindBy(xpath = "/html/body/div[3]/div[2]/div[2]/table/tbody/tr[1]")
+    public WebElement favoriteModule;
+
+    @FindBy(xpath = "/html/body/div[6]/div/ul/li[1]/a/span[2]")
+    public WebElement removeFav;
+
+    @FindBy(xpath = "//a[.='Favorites']")
+    public WebElement favoritesbutton;
+
+    @FindBy (xpath = "//*[@class='filename']//*[@class='innernametext']")
+    public WebElement favFileName;
 
 
 }
--- src/test/java/com/trycloud/step_definitions/US_06_Files_Remove_upload_Raika.java
+++ src/test/java/com/trycloud/step_definitions/US_06_Files_Remove_upload_Raika.java
@@ -3,41 +3,59 @@ package com.trycloud.step_definitions;
 import com.trycloud.pages.BasePage;
 import com.trycloud.pages.LoginPage;
 import com.trycloud.pages.US06Page;
+import com.trycloud.utilities.BrowserUtils;
 import com.trycloud.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.interactions.Actions;
 
 public class US_06_Files_Remove_upload_Raika {
 
     US06Page us06Page = new US06Page();
     LoginPage loginPage = new LoginPage();
+    Actions actions = new Actions(Driver.getDriver());
+
 
 
     @Given("user on the dashboard page")
     public void user_on_the_dashboard_page() {
-        loginPage.login(username,password);
+
+        loginPage.userInput.sendKeys("User2");
+        loginPage.passwordInput.sendKeys("Userpass123");
+        loginPage.logInButton.click();
 
     }
     @When("the user clicks the {string} module")
-    public void the_user_clicks_the_module(String string) {
+    public void the_user_clicks_the_module(String file) {
+        us06Page.filesButton.click();
+        BrowserUtils.sleep(3);
 
     }
     @When("the users click action-icon from any file on the page to remove")
     public void the_users_click_action_icon_from_any_file_on_the_page_to_remove() {
 
+        actions.contextClick(us06Page.favoriteModule).perform();
+        BrowserUtils.sleep(3);
+
     }
     @When("user choose the {string} option")
-    public void user_choose_the_option(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_choose_the_option(String remove) {
+        us06Page.removeFav.click();
+
     }
     @When("user click the {string} sub-module on the left side")
-    public void user_click_the_sub_module_on_the_left_side(String string) {
+    public void user_click_the_sub_module_on_the_left_side(String favorites) {
+        us06Page.favoritesbutton.click();
+        BrowserUtils.sleep(3);
 
     }
     @Then("Verify that the file is removed from the Favorites sub-module’s table")
     public void verify_that_the_file_is_removed_from_the_favorites_sub_module_s_table() {
+      
+
+
 
     }
 }
