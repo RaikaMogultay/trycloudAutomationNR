112a1d5e61cb838cd53d5733fcd9c827d3a8bc5c
RaikaMogultay <raika.engin@gmail.com>
2022-03-30 15:53:05 -0500

    Created new Project

---
+++ src/test/java/com/trycloud/pages/BasePage.java
@@ -0,0 +1,18 @@
+package com.trycloud.pages;
+
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+    public BasePage() {
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "(//a[@aria-label='Files'])[1]")
+    public WebElement filesButton;
+
+
+}
